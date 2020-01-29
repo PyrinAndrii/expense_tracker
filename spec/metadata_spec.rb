207efc7f65bf3ce6d345158c3ea9bd935e80d2c4
Andrii <pyrin.andrii@gmail.com>
2020-01-29 13:57:07 +0200

    Add logger for db

---
+++ spec/metadata_spec.rb
@@ -0,0 +1,9 @@
+require 'pp'
+require 'pry'
+
+RSpec.describe Hash do
+  # it 'is used by RSpec for metadata', :fast, :focus do |example|
+    # binding.pry
+    # pp example.metadata
+  # end
+end
--- spec/support/db.rb
+++ spec/support/db.rb
@@ -3,9 +3,23 @@ RSpec.configure do |c|
     Sequel.extension :migration
     Sequel::Migrator.run(DB, 'db/migrations')
     DB[:expenses].truncate
+
+    FileUtils.mkdir_p('log')
+    require 'logger'
+    # p Logger.new('log/sequel.log')
+    DB.loggers << Logger.new('log/sequel.log')
+
   end
 
-  c.around(:example, :db) do 
-    |example| DB.transaction(rollback: :always) { example.run }
+  c.before(:example, :db) do |example|
+    DB.log_info("Starting example: #{example.description}")
+  end
+
+  c.after(:example, :db) do |example|
+    DB.log_info("Ending example: #{example.description}")
+  end
+
+  c.around(:example, :db) do |example|
+    DB.transaction(rollback: :always) { example.run }
   end
 end
